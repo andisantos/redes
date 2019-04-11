bd4703899084c77ffe28cb776ea181ad82d55dd6
Italo Nicola <italo.niccola@gmail.com>
2019-04-11 05:40:04 +0000

    New timer code with data

--- common.h
+++ common.h
@@ -2,8 +2,8 @@
 #define START_TIMER clock_t t0; t0 = clock();
 #define END_TIMER printf("Time elapsed doing work: %lf\n", (clock() - t0) / (double) CLOCKS_PER_SEC);
 #else
-#define START_TIMER struct timeval t0, t1; gettimeofday(&t0, 0);
-#define END_TIMER gettimeofday(&t1, 0); printf("Time elapsed: %.02lf ms\n", (t1.tv_sec-t0.tv_sec)*1000.0 + (t1.tv_usec-t0.tv_usec)/1000.0);
+#define START_TIMER struct timeval t0, t1; gettimeofday(&t0, 0); printf("T0: %0.03lf\n", (t0.tv_sec)*1000.0 + (t0.tv_usec)/1000.0);
+#define END_TIMER gettimeofday(&t1, 0); printf("T1: %0.03lf\n", (t1.tv_sec)*1000.0 + (t1.tv_usec)/1000.0); printf("Time elapsed: %.03lf ms\n", (t1.tv_sec-t0.tv_sec)*1000.0 + (t1.tv_usec-t0.tv_usec)/1000.0);
 #endif
 
 typedef enum {
